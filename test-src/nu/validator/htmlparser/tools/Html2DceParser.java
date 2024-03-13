ae73c9a84b6727f95cf8dcda6d4efa84f1c682d5
Sasha Firsov <suns@firsov.net>
2024-03-12 17:40:29 -0700

    Html2Dce.java CLI entry created

---
+++ test-src/nu/validator/htmlparser/tools/Html2DceParser.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2008 Mozilla Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package nu.validator.htmlparser.tools;
+
+import nu.validator.htmlparser.common.XmlViolationPolicy;
+import nu.validator.htmlparser.sax.HtmlParser;
+import nu.validator.htmlparser.sax.HtmlSerializer;
+import nu.validator.htmlparser.test.SystemErrErrorHandler;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.*;
+import java.net.MalformedURLException;
+
+public class Html2DceParser {
+
+    /**
+     *
+     * @param in stream ( System.in or FileInputStream )
+     * @param out stream ( System.out or FileOutputStream )
+     * @throws SAXException
+     * @throws ParserConfigurationException
+     * @throws MalformedURLException
+     * @throws IOException
+     * @throws TransformerException
+     */
+    public static void analyze( InputStream in, OutputStream out) throws SAXException,
+            ParserConfigurationException, MalformedURLException, IOException,
+            TransformerException {
+
+        ContentHandler serializer = new HtmlSerializer(out);
+
+        HtmlParser parser = new HtmlParser(XmlViolationPolicy.ALLOW);
+
+        parser.setErrorHandler(new SystemErrErrorHandler());
+        parser.setContentHandler(serializer);
+        parser.setProperty("http://xml.org/sax/properties/lexical-handler",
+                serializer);
+        parser.parse(new InputSource(in));
+        out.flush();
+        out.close();
+    }
+}
